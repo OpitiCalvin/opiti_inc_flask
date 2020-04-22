953488462d71fbde71952c3fadc5372662f760e0
Calvin Opiti <opiticalvin@gmail.com>
2020-04-22 18:13:30 +0200

    JWT config params removed

--- config.py
+++ config.py
@@ -21,9 +21,9 @@ class DevelopmentConfig(Config):
 	
 	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'opiti_inc.sqlite')
 	# POSTGRES = {
-	# 'user': 'spatialadmin',
-	# 'pw': 'sp@t1@l@dm1n',
-	# 'db': 'geofarmer',
+	# 'user': '',
+	# 'pw': '',
+	# 'db': '',
 	# 'host': 'localhost',
 	# 'port': '5432',
 	# }
@@ -31,15 +31,6 @@ class DevelopmentConfig(Config):
 	SECRET_KEY = '3xtr3m3ly-pr1v@t3-@p1-k3y'
 	CUSTOM_DB_FLAG = 'local'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	JWT_TOKEN_LOCATION = ['headers', 'cookies']	# Configure app to store JWTs in cookies
-	JWT_COOKIE_SECURE = False	# Only allow JWT cookies to be sent over https. In production, this should be True
-	JWT_SECRET_KEY = 'jwt-secret-string'
-	JWT_BLACKLIST_ENABLED = True
-	JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
-	JWT_ACCESS_TOKEN_EXPIRES = False
-	JWT_ACCESS_COOKIE_PATH = '/'
-	JWT_REFRESH_COOKIE_PATH = '/'
-	JWT_COOKIE_CSRF_PROTECT = True	# Enable CSRF double submit protection
 	# UPLOADS_DEFAULT_DEST = os.path.join(basedir, 'geofarmer_apis','uploads')
 	# UPLOADED_IMAGES_DEST = os.path.join(basedir, 'geofarmer_apis','uploads', 'logo')
 	# UPLOADED_PHOTOS_DEST = os.path.join(basedir, 'geofarmer_apis','uploads', 'disp_pic')
@@ -52,13 +43,11 @@ class ProductionConfig(Config):
 	
 	TESTING = False
 	SECRET_KEY = uuid.uuid4().hex
-	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'opiti_inc.sqlite')
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'wits_acms.sqlite')
 	# SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
 	CUSTOM_DB_FLAG = 'production'
 	DEBUG = False
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	JWT_SECRET_KEY = ''
-	JWT_ACCESS_TOKEN_EXPIRES = False
 	JSON_SORT_KEYS = False
 	# UPLOADS_DEFAULT_DEST = os.path.join(basedir, 'geofarmer_api','uploads')
 	# UPLOADED_IMAGES_DEST = os.path.join(basedir, 'geofarmer_api','uploads', 'logo')
